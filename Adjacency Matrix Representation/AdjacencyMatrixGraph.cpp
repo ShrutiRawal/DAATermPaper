2e8d7daf21921372b7ff0bd259d6f59edf959678
ShrutiRawal <shruti456rawal@gmail.com>
2020-11-12 20:35:58 +0530

    Code added for adjacency matrix

---
+++ Adjacency Matrix Representation/AdjacencyMatrixGraph.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class AdjMatrixGraph {
+   private:
+  int** adj;     //2-D array to store adjacency matrix
+  int v;         //number of vertices
+
+   public:
+  AdjMatrixGraph(int v) {            //public constructor that
+    this->v = v;                     //initialises the number
+    adj = new int*[v];               //of vertices in the graph and
+    for (int i = 0; i < v; i++) {    //initialises the adjacency matrix
+      adj[i] = new int[v];
+      for (int j = 0; j < v; j++){
+        adj[i][j] = 0;
+      }
+    }
+  }
+
+  void addEdge(int i, int j) {      //utility function to add edges in graph
+    adj[i][j] = 1;
+    adj[j][i]=1;                    //to implement directional property, remove this line
+  }
+
+  void printMatrix() {              //utility function that prints the graph
+    for (int i = 0; i < v; i++) {
+      cout << i << " : ";
+      for (int j = 0; j < v; j++)
+        cout << adj[i][j] << " ";
+      cout << "\n";
+    }
+  }
+
+};
+
+int main() {
+  AdjMatrixGraph graph(5);
+
+  graph.addEdge(0, 1);
+  graph.addEdge(0, 2);
+  graph.addEdge(2, 4);
+  graph.addEdge(1, 3);
+  graph.addEdge(2, 3);
+
+  graph.printMatrix();
+}
